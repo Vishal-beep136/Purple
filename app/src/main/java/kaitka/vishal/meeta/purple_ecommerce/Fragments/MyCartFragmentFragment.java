146d5aefe2b8c15823ef83e3b7d645fb2c49d9a4
Vishal Beep <alphatech@gmail.com>
2021-04-27 12:14:55 +0530

    added price details and cart layout

---
+++ app/src/main/java/kaitka/vishal/meeta/purple_ecommerce/Fragments/MyCartFragmentFragment.java
@@ -0,0 +1,37 @@
+package kaitka.vishal.meeta.purple_ecommerce.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kaitka.vishal.meeta.purple_ecommerce.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class MyCartFragmentFragment extends Fragment {
+
+
+    public MyCartFragmentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_cart_fragment, container, false);
+    }
+}
